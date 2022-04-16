38fd71cb663cd3f3d709af61cd958538946a455b
Nursultan Akhmadov <nur-sultan17@mail.ru>
2022-04-16 04:17:31 +0300

    impl own broadcast with loaded service

--- app/src/main/java/com/nursultan/broadcast/MainActivity.kt
+++ app/src/main/java/com/nursultan/broadcast/MainActivity.kt
@@ -1,13 +1,29 @@
 package com.nursultan.broadcast
 
+import android.content.BroadcastReceiver
+import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ProgressBar
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var progressBar: ProgressBar
     private val broadcastReceiver = MyBroadcastReceiver()
+    private val loadedReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent?) {
+            when (intent?.action) {
+                "loaded" -> {
+                    val percent = intent.getIntExtra("progress", 0)
+                    progressBar.progress = percent
+                }
+            }
+        }
+
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -17,6 +33,7 @@ class MainActivity : AppCompatActivity() {
                 sendBroadcast(this)
             }
         }
+        progressBar = findViewById(R.id.progressBar)
 
         val intentFilter = IntentFilter()
         intentFilter.apply {
@@ -26,6 +43,12 @@ class MainActivity : AppCompatActivity() {
             addAction(MyBroadcastReceiver.ACTION_CLICKED)
         }
         registerReceiver(broadcastReceiver, intentFilter)
+        registerReceiver(loadedReceiver, IntentFilter().apply {
+            addAction("loaded")
+        })
+        Intent(this, MyService::class.java).apply {
+            startService(this)
+        }
     }
 
     override fun onDestroy() {
